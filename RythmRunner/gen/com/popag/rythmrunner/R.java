50ccf1edaa39e356619f35b7c990599db3ac8e4b
Pop Csenge <popcsenge@gmail.com>
2013-10-30 18:46:18 +0100

    Fixed typo in package name.

--- RythmRunner/gen/com/popag/rythmrunner/R.java
+++ RythmRunner/gen/com/popag/rythmrunner/R.java
@@ -40,8 +40,11 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int activity=0x7f050004;
         public static final int app_name=0x7f050000;
+        public static final int former_activity=0x7f050008;
         public static final int hello_world=0x7f050002;
+        public static final int home_image_description=0x7f050007;
         public static final int profile=0x7f050003;
+        public static final int welcome_text=0x7f050006;
         public static final int workout=0x7f050005;
     }
     public static final class style {
--- RythmRunner/src/com/popag/rhythmrunner/HomeActivity.java
+++ RythmRunner/src/com/popag/rhythmrunner/HomeActivity.java
@@ -1,4 +1,6 @@
-package com.popag.rythmrunner;
+package com.popag.rhythmrunner;
+
+import com.popag.rythmrunner.R;
 
 import android.os.Bundle;
 import android.app.Activity;
