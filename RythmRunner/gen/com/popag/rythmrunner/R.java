18f0e7eca1409d7ffcf2fbba418b1a5110512fe6
Pop Csenge <popcsenge@gmail.com>
2013-10-31 00:30:11 +0100

    Populated WorkoutActivity.

--- RythmRunner/gen/com/popag/rythmrunner/R.java
+++ RythmRunner/gen/com/popag/rythmrunner/R.java
@@ -28,18 +28,23 @@ public final class R {
         public static final int running_with_music=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000f;
+        public static final int action_settings=0x7f090014;
         public static final int activityButton=0x7f090001;
         public static final int agePicker=0x7f090006;
         public static final int distance=0x7f09000c;
         public static final int genderSpinner=0x7f090003;
         public static final int heightText=0x7f090004;
         public static final int kcal=0x7f09000a;
+        public static final int playPauseButton=0x7f090012;
         public static final int profileButton=0x7f090000;
+        public static final int resyncButton=0x7f09000f;
         public static final int song_info=0x7f09000d;
         public static final int song_time=0x7f09000e;
         public static final int spm=0x7f090008;
+        public static final int spmDownButton=0x7f090011;
+        public static final int spmUpButton=0x7f090010;
         public static final int stepsText=0x7f090007;
+        public static final int stopButton=0x7f090013;
         public static final int target_spm=0x7f090009;
         public static final int time=0x7f09000b;
         public static final int weightText=0x7f090005;
--- RythmRunner/src/com/popag/rhythmrunner/activity/WorkoutActivity.java
+++ RythmRunner/src/com/popag/rhythmrunner/activity/WorkoutActivity.java
@@ -1,19 +1,104 @@
 package com.popag.rhythmrunner.activity;
 
-import com.popag.rythmrunner.R;
-
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.popag.rythmrunner.R;
 
 /**
  * Activity to display workout data.
- *
+ * 
  * @author Csenge Pop
  */
 public class WorkoutActivity extends Activity {
+	private TextView stepsText;
+	private TextView spmText;
+	private TextView targetSpmText;
+	private TextView kcalText;
+	private TextView timeText;
+	private TextView distanceText;
+	private Button resyncButton;
+	private Button spmUpButton;
+	private Button spmDownButton;
+	private Button stopButton;
+	private Button playPauseButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_workout);
+
+		Context context = (Context) this;
+
+		findViews();
+		addOnclickListeners();
+	}
+
+	private void addOnclickListeners() {
+		resyncButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				buttonPushed(resyncButton);
+			}
+		});
+
+		spmUpButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				buttonPushed(spmUpButton);
+			}
+		});
+
+		spmDownButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				buttonPushed(spmDownButton);
+			}
+		});
+
+		playPauseButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				buttonPushed(playPauseButton);
+			}
+		});
+
+		stopButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				buttonPushed(stopButton);
+			}
+		});
+
+	}
+
+	private void buttonPushed(Button button) {
+		System.out.println("Button: " + button.getText().toString()
+				+ " pushed.");
+	}
+
+	private void findViews() {
+		stepsText = (TextView) findViewById(R.id.stepsText);
+		spmText = (TextView) findViewById(R.id.spm);
+		targetSpmText = (TextView) findViewById(R.id.target_spm);
+		kcalText = (TextView) findViewById(R.id.kcal);
+		timeText = (TextView) findViewById(R.id.time);
+		distanceText = (TextView) findViewById(R.id.distance);
+		resyncButton = (Button) findViewById(R.id.resyncButton);
+		spmUpButton = (Button) findViewById(R.id.spmUpButton);
+		spmDownButton = (Button) findViewById(R.id.spmDownButton);
+		playPauseButton = (Button) findViewById(R.id.playPauseButton);
+		stopButton = (Button) findViewById(R.id.stopButton);
 	}
 }
